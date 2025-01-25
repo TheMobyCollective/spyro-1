dd8de2717cbfe08992f3f5264a530fa49f9f91a4
altro50 <altro50@msn.com>
2025-01-25 05:45:05 +0100

    Added public domain license

--- diff_settings.py
+++
@@ -1,7 +0,0 @@
-def apply(config, args):
-    config["baseimg"] = "build/SCUS_942.28"
-    config["myimg"] = "build/PSX.EXE"
-    config["mapfile"] = "build/psx.map"
-    config["source_directories"] = ["src"]
-    config["arch"] = "mipsel"
-    config["show_line_numbers_default"] = True
